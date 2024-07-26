d2faaaa4a47d43c218803bcba1d1515f5325e028
younicorn777 <yhyun2002@naver.com>
2024-07-26 14:36:25 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 0, b = 0;                    
+	scanf("%d%d", &a, &b);
+
+	printf("%d\n", a * (b % 10));
+	printf("%d\n", a * ((b % 100) / 10));
+	printf("%d\n", a * (b / 100));
+	printf("%d", a * b);
+
+	return 0;
+}
