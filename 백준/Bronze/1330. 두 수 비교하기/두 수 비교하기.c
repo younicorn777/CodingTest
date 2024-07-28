51af698c00e5530601f96689c68472e729753c85
younicorn777 <yhyun2002@naver.com>
2024-07-28 09:23:13 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 0, b = 0;
+    scanf("%d%d", &a, &b);
+
+    if (a > b)
+        printf(">");
+    else if (a < b)
+        printf("<");
+    else
+        printf("==");
+
+    return 0;
+}
