7f6df2237251326c357f650104160649c713914e
younicorn777 <yhyun2002@naver.com>
2024-07-29 12:17:12 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int t = 0; // 테스트 케이스의 개수
+	short a = 0, b = 0; // 두 정수
+	scanf("%d", &t);
+
+	for (int i = 0; i < t; i++)
+	{
+		scanf("%hd%hd", &a, &b);
+		printf("%hd\n", a + b);
+	}
+
+	return 0;
+}
