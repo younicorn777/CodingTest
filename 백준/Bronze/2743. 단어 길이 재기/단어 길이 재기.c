fcbd2dc6b22df11e69f1a442050fba6964543a46
younicorn777 <yhyun2002@naver.com>
2024-08-05 16:43:06 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.c
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.c
@@ -1,12 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
 	char str[100] = { '\0' };
-	short count = 0; // 문자열의 길이
 
 	scanf("%s", str);
-	while (str[count++] != '\0');
-	printf("%hd", count - 1);
+	printf("%zu", strlen(str));
 }
