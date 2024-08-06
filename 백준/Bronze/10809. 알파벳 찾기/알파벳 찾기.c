a659a618dfdbb67d1957b19362a2a27be8d632db
younicorn777 <yhyun2002@naver.com>
2024-08-06 21:36:50 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.c
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.c
@@ -10,17 +10,15 @@ int main(void)
 	for (int i = 0; i < 26; i++)
 		alpha[i] = -1;
 
-	short i = 0;
-	while (s[i] != '\0')
+	for (int j = 97; j <= 122; j++)
 	{
-		for (int j = 97; j <= 122; j++)
+		short i = 0;
+		while (s[i] != '\0')
 		{
 			if ((int)s[i] == j && alpha[j - 97] == -1)
 				alpha[j - 97] = i;
+			i++;
 		}
-		i++;
+		printf("%d ", alpha[j-97]);
 	}
-	
-	for (int k = 0; k < 26; k++)
-		printf("%d ", alpha[k]);
 }
