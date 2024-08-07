0b5a0b23b6bddc7c4049fcb0a9bd885a36eb091c
younicorn777 <yhyun2002@naver.com>
2024-08-07 20:39:38 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.c
+++ 백준/Bronze/2908. 상수/상수.c
@@ -6,16 +6,13 @@ int main(void)
 	char a[4], b[4];
 	scanf("%s%s", a, b);
 
-	for (int i = 0, j = 2, temp1 = 0, temp2 = 0; i < j; i++, j--)
-	{
-		temp1 = a[i];
-		a[i] = a[j];
-		a[j] = temp1;
+	int temp = a[0];
+	a[0] = a[2];
+	a[2] = temp;
 
-		temp2 = b[i];
-		b[i] = b[j];
-		b[j] = temp2;
-	}
+	temp = b[0];
+	b[0] = b[2];
+	b[2] = temp;
 
 	// 각자리 수끼리 비교
 	for (int i = 0; i < 3; i++)
