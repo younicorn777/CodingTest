3cdb23259d6465a4447ec14d60d75672fb69e0aa
younicorn777 <yhyun2002@naver.com>
2024-08-07 19:08:28 +0900

    [Bronze II] Title: 단어의 개수, Time: 12 ms, Memory: 1968 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.c
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.c
@@ -1,26 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
 
 int main() {
-    char word[1000000];
-    int count = 0, len;
-    
-    scanf("%[^\n]s", word);
-    len = strlen(word);
-    
-    if (len == 1 && word[0] == ' ') {
-        printf("%d", count);
-        
+    char str[1000000];
+    int cnt = 0, len;
+
+    scanf("%[^\n]s", str);
+    len = strlen(str);
+
+    if (len == 1 && str[0] == ' ') {
+        printf("%d", cnt);
         return 0;
     }
-    
+
     for (int i = 1; i < len - 1; i++) {
-        if (word[i] == ' ') {
-            count++;
+        if (str[i] == ' ') {
+            cnt++;
         }
     }
-    
-    printf("%d", count + 1);
-    
-    return 0;
+
+    printf("%d", cnt + 1);
 }
