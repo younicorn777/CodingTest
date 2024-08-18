705b09e0b1ac66838c643c5d4f92f21234316ffb
younicorn777 <yhyun2002@naver.com>
2024-08-18 09:47:02 +0900

    [Bronze I] Title: 진법 변환 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.c
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.c
@@ -12,16 +12,10 @@ int main(void)
 		remain = result % b; // 정수끼리의 연산에서만 %가능, 결과값 또한 정수
 		result /= b;         // 정수끼리의 연산이므로, 결과값 정수
 
-		if (remain >= 10 && remain <= 35)
-			num[i++] = remain + 55;
-		else if(remain >= 0 && remain <= 9)
-			num[i++] = remain + 48;
+		num[i++] = (remain >= 10) ?  remain + 55 : remain + 48;
 	}
 	
-	if (result >= 10 && result <= 35)
-		num[i] = result + 55;
-	else if (result >= 0 && result <= 9)
-		num[i] = result + 48;
+	num[i] = (result >= 10) ? result + 55 : result + 48;
 
 	for (int j = i; j >= 0; j--)
 		printf("%c", num[j]);
