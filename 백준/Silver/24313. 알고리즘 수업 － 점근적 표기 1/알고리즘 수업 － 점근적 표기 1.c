dd061881a374e109ea71877227c40b8529c5bc83
younicorn777 <yhyun2002@naver.com>
2024-09-04 17:30:11 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int a0, a1, c, n0;
+	scanf("%d%d%d%d", &a1, &a0, &c, &n0);
+
+	if (a0 < 0 && a1 > c)
+		printf("0");
+	else
+	{
+		if (((c - a1) * n0 - a0) >= 0)
+			printf("1");
+		else
+			printf("0");
+	}
+}
