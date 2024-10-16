4786ecace902f8ad4ed8d66d258b3b9925dac1b9
younicorn777 <yhyun2002@naver.com>
2024-10-16 12:32:35 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.c
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.c
@@ -1,63 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
-
-void MergeTwoArr(char arr[], int left, int mid, int right)
-{
-	int fidx = left;
-	int ridx = mid + 1;
-	char* SortArr = (char*)malloc(sizeof(char) * (right + 1));
-	int sidx = left;
-
-	while (fidx <= mid && ridx <= right)
-	{
-		if (arr[fidx] > arr[ridx])
-			SortArr[sidx++] = arr[fidx++];
-		else
-			SortArr[sidx++] = arr[ridx++];
-	}
-
-	if (fidx > mid)
-	{
-		for (int i = ridx; i <= right; i++)
-			SortArr[sidx++] = arr[i];
-	}
-	else
-	{
-		for (int i = fidx; i <= mid; i++)
-			SortArr[sidx++] = arr[i];
-	}
-
-	for (int i = left; i <= right; i++)
-		arr[i] = SortArr[i];
-
-	free(SortArr);
-}
-
-void MergeSort(char arr[], int left, int right)
-{
-	int mid;
-
-	if (left < right)
-	{
-		mid = (left + right) / 2;
-		MergeSort(arr, left, mid);
-		MergeSort(arr, mid + 1, right);
-		MergeTwoArr(arr, left, mid, right);
-	}
-}
 
 int main(void)
 {
-	char arr[11];
-	int n, size = 0; 
+	int arr[11];
+	int CountArr[10] = { 0 };
+
+	int n, size = 0;
 	scanf("%d", &n);
 
 	for (int i = n; i > 0; i /= 10)
-		arr[size++] = i % 10 - '0';
+	{
+		arr[size++] = i % 10;
+		CountArr[arr[size - 1]]++;
+	}
 
-	MergeSort(arr, 0, size - 1);
-
-	for (int i = 0; i < size; i++)
-		printf("%d", arr[i] + '0');
+	for (int i = 9; i >= 0; i--)
+	{
+		for (int j = 0; j < CountArr[i]; j++)
+		{
+			printf("%d", i);
+		}
+	}
 }
