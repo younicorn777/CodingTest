01644a5065531da2ebb9484bdc27e9ba0efac992
younicorn777 <yhyun2002@naver.com>
2024-10-31 20:50:42 +0900

    [Silver IV] Title: 제로, Time: 12 ms, Memory: 4152 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.c
@@ -0,0 +1,80 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int Data;
+
+typedef struct _node
+{
+	Data data;
+	struct _node* next;
+}Node;
+
+typedef struct _list_stack
+{
+	Node* head;
+}Stack;
+
+void StackInit(Stack* pstack)
+{
+	pstack->head = (Node*)malloc(sizeof(Node));
+	pstack->head->next = NULL;
+}
+
+int SIsEmpty(Stack* pstack)
+{
+	if (pstack->head->next == NULL)
+		return TRUE;
+	else
+		return FALSE;
+}
+
+void SPush(Stack* pstack, Data data)
+{
+	Node* newnode = (Node*)malloc(sizeof(Node));
+	newnode->data = data;
+
+	newnode->next = pstack->head->next;
+	pstack->head->next = newnode;
+}
+
+Data SPop(Stack* pstack)
+{
+	Node* delnode;
+	Data deldata;
+
+	if (SIsEmpty(pstack))
+		return -1;
+
+	delnode = pstack->head->next;
+	deldata = delnode->data;
+
+	pstack->head->next = delnode->next;
+	free(delnode);
+	return deldata;
+}
+
+
+int main(void)
+{
+	Stack stack;
+	StackInit(&stack);
+
+	int k, result = 0;
+	scanf("%d", &k);
+
+	for (int i = 0, data; i < k; i++)
+	{
+		scanf("%d", &data);
+
+		(data == 0) ? SPop(&stack) : SPush(&stack, data);
+	}
+
+	while (!SIsEmpty(&stack))
+		result += SPop(&stack);
+
+	printf("%d", result);
+}
