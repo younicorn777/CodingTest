8cbc4d3fe48e1922e328e558c621abb1f5f44ddf
younicorn777 <yhyun2002@naver.com>
2025-08-12 09:03:27 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,29 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> v, b;
-int temp;
-void test() {
-	int sum = accumulate(b.begin(), b.end(), 0);
-	if (sum == 100) {
-		sort(b.begin(), b.end());
-		for (int i : b) cout << i << '\n';
-		exit(0);
-	}
-}
-void cobi(int start) {
-	if (b.size() == 7) test();
-	for (int i = start + 1; i < 9; i++) {
-		b.push_back(v[i]);
-		cobi(i);
-		b.pop_back();
-	}
-}
+int a[9];
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-	for(int i = 0; i < 9; i++){
-		cin >> temp;
-		v.push_back(temp);
-	}
-	cobi(-1);
+	for (int i = 0; i < 9; i++) cin >> a[i];
+	sort(a, a + 9);
+	do {
+		int sum = 0;
+		for (int i = 0; i < 7; i++) sum += a[i];
+		if (sum == 100) break;
+	} while (next_permutation(a, a + 9));
+	for (int i = 0; i < 7; i++)cout << a[i] << '\n';
 }
