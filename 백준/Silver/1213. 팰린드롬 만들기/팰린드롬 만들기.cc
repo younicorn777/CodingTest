8a0e9f6b249367d73270ba41274078a0824cb991
younicorn777 <yhyun2002@naver.com>
2025-08-19 18:58:28 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -1,32 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-int alpha[26], ocnt, oidx = -1;
-string s;
-deque<char> d;
-bool test(){
-	for (int i = 0; i < 26; i++) {
-		if (alpha[i] % 2 != 0) {
-			ocnt++;
-			oidx = i;
-		}
-	}
-	return ocnt <= (s.size() % 2);
-}
+string s, ret;
+int cnt[200], flag;
+char mid;
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 	cin >> s;
-	for (char c : s) alpha[c - 'A']++;
-	if (test()) {
-		if(oidx != -1) d.push_back(oidx + 'A');
-		for (int i = 25; i >= 0; i--) {
-			for (int j = 0; j < (alpha[i] / 2); j++) {
-				d.push_front(i + 'A');
-				d.push_back(i + 'A');
+	for (char c : s) cnt[c]++;
+	for (int i = 'Z'; i >= 'A'; i--) {
+		if (cnt[i]) {
+			if (cnt[i] & 1) {
+				mid = (char)i; flag++;
+				cnt[i]--;
+			}
+			if (flag == 2) break;
+			for (int j = 0; j < cnt[i]; j += 2) {
+				ret = char(i) + ret;
+				ret += char(i);
 			}
 		}
-		for (char c : d) cout << c;
-		return 0;
 	}
-	else cout << "I'm Sorry Hansoo";
+	if (mid) ret.insert(ret.begin() + ret.size() / 2, mid);
+	if (flag == 2) cout << "I'm Sorry Hansoo";
+	else cout << ret;
 }
