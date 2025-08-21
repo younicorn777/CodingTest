be1c656ef0eee4fa4fcf104995fe9587b670de2c
younicorn777 <yhyun2002@naver.com>
2025-08-21 14:20:53 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2240 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int dy[] = { -1,0,1,0 };
+const int dx[] = { 0,1,0,-1 };
+int a[104][104], visited[104][104], n ,m, y, x;
+string s;
+void BFS() {
+	queue<pair<int, int>> q;
+	q.push({ 0,0 });
+	visited[0][0] = 1;
+	while (!q.empty()) {
+		tie(y, x) = q.front(); q.pop();
+		for(int i = 0; i < 4; i++){
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+			if (a[ny][nx] && !visited[ny][nx]) {
+				visited[ny][nx] = visited[y][x] + 1;
+				q.push({ ny,nx });
+			}
+		}
+	}
+}
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		for (int j = 0; j < m; j++) a[i][j] = s[j] - '0';
+	}
+	BFS();
+	cout << visited[n - 1][m - 1];
+}
