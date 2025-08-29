4c7511ee8dd32232223dfed178743155c60ae828
younicorn777 <yhyun2002@naver.com>
2025-08-29 16:25:01 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

---
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int max_n = 68;
+int a[max_n][max_n], n;
+string s;
+void go(int y, int x, int size) {
+	int zcnt = 0, ocnt = 0;
+	for (int i = y; i < y + size; i++) {
+		for (int j = x; j < x + size; j++) {
+			if (a[i][j]) ocnt++;
+			else zcnt++;
+		}
+	}
+	if (zcnt > 0 && ocnt == 0) {
+		cout << '0';
+		return;
+	}
+	else if (zcnt == 0 && ocnt > 0) {
+		cout << '1';
+		return;
+	}
+	else {
+		cout << '(';
+		go(y, x, size / 2);
+		go(y, x + size / 2, size / 2);
+		go(y + size / 2, x, size / 2);
+		go(y + size / 2, x + size / 2, size / 2);
+		cout << ')';
+	}
+}
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		for (int j = 0; j < n; j++) a[i][j] = s[j] - '0';
+	}
+	go(0, 0, n);
+}
