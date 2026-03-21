7813b871052aa08e4fcae34966061def2de56293
younicorn777 <yhyun2002@naver.com>
2026-03-21 14:20:25 +0900

    [Silver IV] Title: 덱, Time: 68 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -1,64 +1,20 @@
+import collections
 import sys
+
 input = sys.stdin.readline
 
-class CircularDeque:
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.array = [None] * capacity
-        self.f = 0  
-        self.r = 0 
-
-    def isEmpty(self):
-        return 1 if self.f == self.r else 0
-
-    def isFull(self):
-        return 1 if self.f == (self.r + 1) % self.capacity else 0
-
-    def push_front(self, item):
-        if not self.isFull():
-            self.array[self.f] = item
-            self.f = (self.f - 1 + self.capacity) % self.capacity
-
-    def push_back(self, item):
-        if not self.isFull():
-            self.r = (self.r + 1) % self.capacity
-            self.array[self.r] = item
-
-    def pop_front(self):
-        if self.isEmpty(): return -1
-        self.f = (self.f + 1) % self.capacity
-        return self.array[self.f]
-
-    def pop_back(self):
-        if self.isEmpty(): return -1
-        item = self.array[self.r]
-        self.r = (self.r - 1 + self.capacity) % self.capacity
-        return item
-
-    def size(self):
-        return (self.r - self.f + self.capacity) % self.capacity
-
-    def front(self):
-        if self.isEmpty(): return -1
-        return self.array[(self.f + 1) % self.capacity]
-
-    def back(self): 
-        if self.isEmpty(): return -1
-        return self.array[self.r]
-
-
 n = int(input())
-dq = CircularDeque(10005) 
+dq = collections.deque() 
 
 for _ in range(n):
     line = input().split()
     cmd = line[0]
 
-    if cmd == "push_front":  dq.push_front(int(line[1]))
-    elif cmd == "push_back": dq.push_back(int(line[1]))
-    elif cmd == "pop_front": print(dq.pop_front())
-    elif cmd == "pop_back":  print(dq.pop_back())
-    elif cmd == "size":      print(dq.size())
-    elif cmd == "empty":     print(dq.isEmpty())
-    elif cmd == "front":     print(dq.front())
-    elif cmd == "back":      print(dq.back())
+    if cmd == "push_front":  dq.appendleft(int(line[1]))
+    elif cmd == "push_back": dq.append(int(line[1]))
+    elif cmd == "pop_front": print(dq.popleft() if dq else -1)     
+    elif cmd == "pop_back":  print(dq.pop() if dq else -1)     
+    elif cmd == "size":      print(len(dq))
+    elif cmd == "empty":     print(0 if dq else 1)
+    elif cmd == "front":     print(dq[0] if dq else -1)
+    elif cmd == "back":      print(dq[-1] if dq else -1)  
