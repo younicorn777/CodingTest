eaa103270f3ae02b9273c0da362f85544f8faad4
younicorn777 <yhyun2002@naver.com>
2026-05-17 11:20:47 +0900

    [level 3] Title: 여행경로, Time: 215.09 ms, Memory: 13.6 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -11,23 +11,14 @@ def solution(tickets):
             return
         
         # 연결된 항공권 탐색
-        for idx, ticket in enumerate(tickets):
+        for idx, ticket in enumerate(tickets):   
             
             # 출발 공항이 같고 아직 방문하지 않은 경우
             if ticket[0] == airport and not visited[idx]:
-                
-                visited[idx] = True
-                
-                # 다음 공항 탐색
-                dfs(ticket[1], path + [ticket[1]])
-                
-                # 백트래킹
-                visited[idx] = False
+                visited[idx] = True # 방문 표시
+                dfs(ticket[1], path + [ticket[1]]) # 다음 공항 탐색
+                visited[idx] = False # 백트래킹
     
-    # ICN 공항부터 탐색 시작
-    dfs("ICN", ["ICN"])
-    
-    # 사전순 정렬
-    answer.sort()
-    
+    dfs("ICN", ["ICN"]) # ICN 공항부터 탐색 시작
+    answer.sort() # 사전순 정렬
     return answer[0]
